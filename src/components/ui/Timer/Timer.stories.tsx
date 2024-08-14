2c046c4f5ddd16cc5bea3d0a5df96f6d08e2228c
kirillllllcmirnov <kiryusha.smirnov@internet.ru>
2024-08-14 14:08:36 +0300

    Base

--- src/components/ui/Timer/Timer.stories.tsx
+++ src/components/ui/Timer/Timer.stories.tsx
@@ -1,10 +1,13 @@
+import type {Meta, StoryObj} from "@storybook/react"
 import { Timer } from "./Timer"
 import "./timer.module.css"
 
-export default {
-    title:"UI/Timer",
+const meta:Meta<typeof Timer> = {
     component:Timer,
 
 }
-
-export const Default = () => <Timer />
+export default meta
+type Story = StoryObj<typeof Timer>
+export const Primary:Story = {
+    args:{}
+}
--- src/components/ui/Timer/Timer.tsx
+++ src/components/ui/Timer/Timer.tsx
@@ -1,8 +1,17 @@
 import { useEffect, useMemo, useState } from "react"
 import styles from "./timer.module.css"
-export function Timer(){
+
+export type TimerTypes = {
+    color?:string,
+    strokeColor?:string,
+    strokeWidth?:string,
+    strokeBg?:string
+    
+}
+export function Timer({color,strokeColor,strokeWidth,strokeBg}:TimerTypes){
     let [date,setDate] = useState(new Date())
     let [progress,setProgress] = useState(0)
+    console.log(color,strokeColor,strokeWidth)
     useEffect(()=>{
         let timer = setInterval(()=>{
             setDate(new Date())
@@ -17,10 +26,10 @@ export function Timer(){
     return (
         <div className={styles.body}>
             <svg width="100" height="100">
-                <circle cx="50" cy="50" r="45" fill="none" style={{stroke:""}} className={styles.bgcircle}/>
-                <circle cx="50" cy="50" r="45" fill="none" style={{stroke:""}} strokeDasharray={283} strokeDashoffset={progress} className={styles.circle}/>
+                <circle cx="50" cy="50" r="45" fill="none" stroke={strokeBg ?? "gainsboro"} strokeWidth={strokeWidth ?? "5px"} />
+                <circle cx="50" cy="50" r="45" fill="none" stroke={strokeColor ?? "black"} strokeWidth={strokeWidth ?? "5px"}   strokeDasharray={283} strokeDashoffset={progress} className={styles.circle}/>
             </svg>
-            <div className={styles.timer}>
+            <div className={styles.timer} style={{color:color ?? "black"}}>
                 {date.toLocaleTimeString()}
             </div>
         </div>
